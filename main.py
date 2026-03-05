a22577c5dc7cb730b4777bef5c0c1875c871ff51
Andrew Tsaprounis <andrew.tsaprounis@education.nsw.gov.au>
2026-03-05 12:03:54 +0000

    final

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
 from flask import request
+from flask import session
+from flask import url_for
 from flask_wtf.csrf import CSRFProtect
 
 import user_management as dbHandler
@@ -17,14 +20,15 @@ CSRFProtect(app)
 
 @app.route("/success.html", methods=["POST", "GET"])
 def addFeedback():
+    if "username" not in session:
+        return redirect(url_for("home", msg="Please log in first."))
+
     if request.method == "POST":
         feedback = request.form["feedback"]
         dbHandler.insertFeedback(feedback)
-        dbHandler.listFeedback()
-        return render_template("/success.html", state=True, value="Back")
-    else:
-        dbHandler.listFeedback()
-        return render_template("/success.html", state=True, value="Back")
+
+    dbHandler.listFeedback()
+    return render_template("/success.html", state=True, value=session["username"])
 
 
 @app.route("/signup.html", methods=["POST", "GET"])
@@ -34,9 +38,15 @@ def signup():
         password = request.form["password"]
         DoB = request.form["dob"]
         dbHandler.insertUser(username, password, DoB)
-        return render_template("/index.html")
-    else:
-        return render_template("/signup.html")
+        return redirect(url_for("home", msg="Signup successful. Please log in."))
+
+    return render_template("/signup.html", state=("username" in session))
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("home", msg="You have been logged out."))
 
 
 @app.route("/index.html", methods=["POST", "GET"])
@@ -44,18 +54,19 @@ def signup():
 def home():
     if request.method == "GET":
         msg = request.args.get("msg", "")
-        return render_template("/index.html", msg=msg)
-    elif request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        isLoggedIn = dbHandler.retrieveUsers(username, password)
-        if isLoggedIn:
-            dbHandler.listFeedback()
-            return render_template("/success.html", value=username, state=isLoggedIn)
-        else:
-            return render_template("/index.html")
-    else:
-        return render_template("/index.html")
+        return render_template("/index.html", msg=msg, state=("username" in session))
+
+    username = request.form["username"]
+    password = request.form["password"]
+    isLoggedIn = dbHandler.retrieveUsers(username, password)
+    if isLoggedIn:
+        session["username"] = username
+        dbHandler.listFeedback()
+        return render_template("/success.html", value=username, state=True)
+
+    return render_template(
+        "/index.html", msg="Invalid username or password.", state=False
+    )
 
 
 if __name__ == "__main__":
